d287091d1e416c270787eacbe4b37f3206a98a80
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-04-21 09:12:04 -0600

    feat: creating structure to create a custom plugin

---
+++ wp-content/plugins/custom-crud-plugin/init.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Custom crud plugin
+Description: Plugin para crear nuevas tablas en la base de datos y crear un crud de estas.
+Version: 1.0.0
+Author: Fernando Hernández Paz
+Author URI: -
+*/
