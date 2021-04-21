c84091c5e7a50c084c593762e4ae791a7423948f
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-04-21 14:11:19 -0600

    feat: agregando html para listad de causas de muerte

---
+++ wp-content/plugins/custom-crud-plugin/causas_muerte_list.php
@@ -0,0 +1,63 @@
+<?php
+
+function list_causas_muerte()
+{
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <?php
+    global $wpdb;
+    $table_name = $wpdb->prefix . "causas_muerte";
+    $data = $wpdb->get_results("SELECT id, user_id, descripcion, abreviatura, activo from $table_name");
+    $columnas = $wpdb->get_col("DESC {$table_name}", 0);
+    $columnasOcultas = ['fecha_registro'];
+    ?>
+    <div class="wrap">
+        <div class="container-fluid" style="background: white">
+            <div class="row">
+                <div class="col-lg-9 col-md-9 col-sm-9 col-12">
+                    <h4>Lista de causas de muerte</h4>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-4">
+                    <a href=""
+                       role="button" class="btn btn-outline-primary">
+                        Agregar nueva causa muerte
+                    </a>
+                </div>
+            </div>
+            <div class="row mt-1">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                    <table class="table table-sm table-bordered w-100">
+                        <thead>
+                        <tr>
+                            <?php
+                            foreach ($columnas as $columna) {
+                                if (!in_array($columna, $columnasOcultas)) {
+                                    ?>
+                                    <th class="text-center text-uppercase">
+                                        <?php echo str_replace('_id', '', $columna) ?>
+                                    </th>
+                                <?php }
+                            } ?>
+                            <th>Acciones</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php if (empty($data)) { ?>
+                            <tr>
+                                <td colspan="<?php echo count($columnas) ?>" class="text-center font-weight-bold">
+                                    No se encontraron datos registrados
+                                </td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+            crossorigin="anonymous"></script>
+    <?php
+}
--- wp-content/plugins/custom-crud-plugin/init.php
+++ wp-content/plugins/custom-crud-plugin/init.php
@@ -7,6 +7,12 @@ Author: Fernando Hernández Paz
 Author URI: -
 */
 
+// definiendo directorio en base al archivo init
+define('ROOTDIR', plugin_dir_path(__FILE__));
+
+// agregando nuevas funciones
+require_once(ROOTDIR . 'causas_muerte_list.php');
+
 // if you want to check in debug log
 // require_once(ROOTDIR . 'write_in_log.php');
 
@@ -14,8 +20,7 @@ Author URI: -
 function creating_news_tables()
 {
     global $wpdb;
-    $charset_collate = $wpdb->get_charset_collate();
-    $path = ABSPATH . 'wp-content/plugins/custom-crud-plugin/script.sql';
+    $path = ROOTDIR . '/script.sql';
     $file = fopen($path, 'r');
     $sql_file = fread($file, filesize($path));
     fclose($file);
@@ -26,3 +31,61 @@ function creating_news_tables()
 
 // run the install scripts upon plugin activation
 register_activation_hook(__FILE__, 'creating_news_tables');
+
+//menu items
+add_action('admin_menu', 'agregarNuevasOpcionesMenu');
+function agregarNuevasOpcionesMenu()
+{
+    $configMenu = [
+        'menu' => [
+            [
+                'page_title' => 'Estadisticas',
+                'menu_title' => 'Estadisticas',
+                'menu_slug' => 'lista_estadisticas',
+                'function' => 'list_causas_muerte',
+            ]
+        ],
+        'sub_menu' => [
+
+        ]
+    ];
+    $keys = array_keys($configMenu);
+    foreach ($keys as $key) {
+        if ($key === 'menu') {
+            foreach ($configMenu[$key] as $option) {
+                add_menu_page(
+                    $option['page_title'],
+                    $option['menu_title'],
+                    'manage_options',
+                    $option['menu_slug'],
+                    $option['function']
+                );
+            }
+        }
+    }
+    //this is the main item for the menu
+//    add_menu_page('Schools', //page title
+//        'Schools', //menu title
+//        'manage_options', //capabilities
+//        'sinetiks_schools_list', //menu slug
+//        'sinetiks_schools_list' //function
+//    );
+//
+//    //this is a submenu
+//    add_submenu_page('sinetiks_schools_list', //parent slug
+//        'Add New School', //page title
+//        'Add New', //menu title
+//        'manage_options', //capability
+//        'sinetiks_schools_create', //menu slug
+//        'sinetiks_schools_create'); //function
+//
+//    //this submenu is HIDDEN, however, we need to add it anyways
+//    add_submenu_page(null, //parent slug
+//        'Update School', //page title
+//        'Update', //menu title
+//        'manage_options', //capability
+//        'sinetiks_schools_update', //menu slug
+//        'sinetiks_schools_update'); //function
+}
+
+
