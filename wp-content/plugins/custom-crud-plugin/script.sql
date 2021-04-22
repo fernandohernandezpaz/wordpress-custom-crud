cdcc816666eb39f46ff2051fb8d9ca500f923bd0
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-04-22 15:45:28 -0600

    del: eliminando antigua carpeta plugin

--- wp-content/plugins/custom-crud-plugin/script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `{prefix}causas_muerte`(
-    `id` BIGINT PRIMARY KEY auto_increment,
-    `user_id` BIGINT,
-    `descripcion` varchar(255),
-    `abreviatura` varchar(255) null,
-    `activo` tinyint,
-    `fecha_registro` datetime
-);
-
